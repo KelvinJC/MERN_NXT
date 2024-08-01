f0bf85c82eac6d47cd4e6d1b97f51202f5d4aab2
Kelvin <kelvin.johnson-chu@abujaelectricity.com>
2024-08-01 18:53:03 +0100

    Understand modules in Node.js

---
+++ john-smilga/1-practice/1-global_variables.js
@@ -0,0 +1,20 @@
+// GLOBALS - NO WINDOW
+
+// __dirname  - path to current directory
+// __filename - file name
+// require    - function to use modules (CommonJS)
+// module     - info about current module (file)
+// process    - info about env where program is executed
+// console 
+
+
+// console.log(__dirname)
+
+// console.log(__filename)
+
+// console.log(require)
+
+// console.log("module \n", module)
+
+
+console.log("process \n", process)
