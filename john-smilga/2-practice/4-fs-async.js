b11de490dde2cec822625cf8205710aeb589daaa
Kelvin <kelvin.johnson-chu@abujaelectricity.com>
2024-08-01 20:48:14 +0100

    Perform async file ops

---
+++ john-smilga/2-practice/4-fs-async.js
@@ -0,0 +1,65 @@
+const { readFile, writeFile } = require('fs');
+
+readFile('./content/first.txt', 'utf8', (err, result) => {
+    if (err) {
+        console.log(err);
+        return;
+    }
+    console.log(result);
+    const first = result; 
+    readFile('./content/second.txt', 'utf8', (err, result) => {
+        if (err) {
+            console.log(err);
+            return;
+        }
+        console.log(result);
+        const second = result; 
+        writeFile(
+            './content/result-async.txt', 
+            `Here lies the async result typed for ${first} and ${second}`,
+            (err, result) => {
+                if (err) {
+                    console.log(err);
+                    return;
+                }
+                console.log("WRITE result", result);            
+            },
+        );
+    })
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var first;
+
+// function setArg(fileContent) {
+//     first = fileContent;
+// }
+
+// readFile('./content/first.txt', 'utf8', (err, result) => {
+//     if (err) {
+//         console.log(err);
+//         return
+//     }
+//     // console.log(result);
+//     console.log("first", result)
+//     setArg(result);
+// })
+
+// console.log("second", first)
