75940b41f15b45a29675ebe1757f98084d979db5
Kelvin <kelvin.johnson-chu@abujaelectricity.com>
2024-08-01 20:48:37 +0100

    Perform async file ops with async-await

---
+++ john-smilga/2-practice/5-fs-async-await.js
@@ -0,0 +1,15 @@
+const { readFile, writeFile } = require('fs').promises;
+
+const t = async () => {
+    try {
+        const first = await readFile('./content/first.txt', 'utf8');
+        const second = await readFile('./content/second.txt', 'utf8');
+    
+        console.log(first);
+    } catch (error) {
+        console.log(error);
+    }
+
+}
+
+t();
