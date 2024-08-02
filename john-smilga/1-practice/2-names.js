338b4dd83dd8a797541f8bc6469a09e4c123af15
Kelvin <kelvin.johnson-chu@abujaelectricity.com>
2024-08-02 11:52:16 +0100

    Rename files and dirs

--- john-smilga/1-practice/2-names.js
+++
@@ -1,8 +0,0 @@
-// Modules - Encapsulated code (only share minimum)
-const secret = 'SUPER SECRET'
-const john = 'johnny'
-const peter = 'pierre'
-
-
-module.exports = { john, peter }
-console.log(module)
--- john-smilga/5-practice/event_loop_set_interval.js
+++
@@ -1,8 +0,0 @@
-console.log('first')
-setInterval(()=> {
-    console.log("Hey!");
-}, 2000);
-console.log("send message");
-// process stays alive unless 
-// killed by CTRL + C
-// or unexpected error
