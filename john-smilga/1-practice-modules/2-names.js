02fb40bc0e43d1374dbb26676fa8cf17ee44f368
Kelvin <kelvin.johnson-chu@abujaelectricity.com>
2024-08-02 11:45:27 +0100

    Rename module to practice modules

---
+++ john-smilga/1-practice-modules/2-names.js
@@ -0,0 +1,8 @@
+// Modules - Encapsulated code (only share minimum)
+const secret = 'SUPER SECRET'
+const john = 'johnny'
+const peter = 'pierre'
+
+
+module.exports = { john, peter }
+console.log(module)
